5a74d0a8d083ac8a6b74a4a3dc1550a2d9eac04b
Anton Ahl <antonahl@protonmail.com>
2023-02-01 16:20:05 +0100

    Remove .pre-* files after they are used

--- khoon.sh
+++ khoon.sh
@@ -7,4 +7,6 @@ input="$1"
 shift
 python3 parser.py $input > $input.pre-parsed
 krun $input.pre-parsed "$@" > $input.pre-pretty
-python3 pretty.py $input.pre-pretty
+output=$(python3 pretty.py $input.pre-pretty)
+rm $input.pre-parsed $input.pre-pretty
+echo $output
