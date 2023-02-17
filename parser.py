d29c63acf70412f379cd655319927ac3277830f4
Anton Ahl <antonahl@protonmail.com>
2023-02-17 17:05:28 +0100

    Replace . with \dot only for the current subject

--- parser.py
+++ parser.py
@@ -11,14 +11,30 @@ def explicit_gaps_and_aces(s):
 def explicit_aces(s):
     return re.sub(r' ', r'\\ace', s)
 
-# Replace dots "." with \dot in subject expressions only
+# Replace '.' with '\dot' if it refers to the current subject
 def replace_dots(s):
-    matches = re.findall(r'([^0-9%]\.+[^0-9=])|(^\.+[^0-9=])|([^0-9%]\.+$)|(^\.$)', s)
-    for tup in matches:
+    dots_to_keep = re.findall(r'([0-9]\.+[0-9])|([%]\.+[yn])|(\.[=])|([a-z]\.[a-z])', s)
+    for tup in dots_to_keep:
         match = ''.join(tup)
-        dots = match.replace(".", "\\dot")
-        s = s.replace(match, dots, 1)
+        new = match.replace(".", "\\tmpdot")
+        s = s.replace(match, new, 1)
+    dot_sequences = re.findall(r'\.{2,}', s)
+    for dot_sequence in dot_sequences:
+        new = alt_subst(dot_sequence, "\\tmpdot")
+        s = s.replace(dot_sequence, new, 1)
+    s = s.replace(".", "\\dot")
+    s = s.replace("\\tmpdot", ".")
     return s
+
+# Substitute every other character in s1 with s2
+def alt_subst(s1, s2):
+    new = []
+    for i in range(0, len(s1)):
+        if i % 2:
+            new.append(s2)
+        else:
+            new.append(s1[i])
+    return ''.join(new)
 
 input_file = sys.argv[1]
 with open(input_file) as f:
