bd44ef65d53f27ceefd5bed80f8aeff9d5487827
Tamás Tóth <tothtamas28@users.noreply.github.com>
2023-05-26 14:31:49 +0200

    Refactor `pretty.py`

--- src/khoon/pretty.py
+++ src/khoon/pretty.py
@@ -2,28 +2,36 @@ import re
 import sys
 
 
+def pretty(text: str) -> str:
+    res = _pretty_spaces(text)
+    res = _pretty_dots(res)
+    res = _get_k_res(res)
+    res = _strip_meta_info(res)
+    return res
+
+
 # Remove all spaces and replace aces with spaces
-def pretty_spaces(s: str) -> str:
+def _pretty_spaces(s: str) -> str:
     s = re.sub(r'\\gap| ', '', s)
     s = re.sub(r'\\ace', ' ', s)
     return s
 
 
 # Replace all '\dot' with '.'
-def pretty_dots(s: str) -> str:
+def _pretty_dots(s: str) -> str:
     s = re.sub(r'\\dot', '.', s)
     return s
 
 
 # Get the value of the k cell
-def get_k_res(s: str) -> str:
+def _get_k_res(s: str) -> str:
     lines = s.splitlines()
     i = lines.index('<k>') + 1
     return lines[i][:-3]  # remove ending '~>.'
 
 
 # Extract the noun from the meta noun
-def strip_meta_info(s: str) -> str:
+def _strip_meta_info(s: str) -> str:
     i = s.find(',')
     if i == -1:
         return s
@@ -31,11 +39,9 @@ def strip_meta_info(s: str) -> str:
         return s[i + 1 : -1]
 
 
-input_file = sys.argv[1]
-with open(input_file) as f:
-    content = f.read()
-res = pretty_spaces(content)
-res = pretty_dots(res)
-res = get_k_res(res)
-res = strip_meta_info(res)
-print(res)
+if __name__ == '__main__':
+    input_file = sys.argv[1]
+    with open(input_file) as f:
+        text = f.read()
+    res = pretty(text)
+    print(res)
